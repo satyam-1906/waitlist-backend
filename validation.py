6d2a5cbe5a6cf0d6d5b92963a54a0fc48a54b4f8
satyam-1906 <scientificsatyam1906@gmail.com>
2026-07-15 15:58:01 +0530

    bug fix

--- validation.py
+++ validation.py
@@ -2,6 +2,6 @@ from database import formData
 
 def user_exists(email, number, db) -> bool:
     user = db.query(formData).filter_by(email=email).first()
-    if user is not None and user.number == number:
+    if user is not None and user.mobile_number == number:
         return False
     return True
